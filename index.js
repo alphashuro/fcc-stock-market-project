4383c54fef7a7b853a31bcb259565037f8162e8a
Alpha Shuro <alphashuro@gmail.com>
2016-04-23 20:31:15 +0200

    convert to es5 for heroku

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const _ = require('underscore');
+'use strict';
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var _ = require('underscore');
 
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 app.use(express.static('app'));
 
-server.listen(8000, () => console.log('Listening at 8000'));
+var port = process.env.PORT || 8080;
+
+server.listen(port, function () {
+  return console.log('Listening at docker:'+port);
+});
 
 var stocks = [];
 
-io.on('connection', socket => {
-  socket.emit('initial-stocks', stocks );
-  socket.on('add-stock', code => {
-  	console.log('adding '+code+' to '+stocks);
-  	stocks.push(code);
-  	io.emit('add-stock', code );
+io.on('connection', function (socket) {
+  socket.emit('initial-stocks', stocks);
+  socket.on('add-stock', function (code) {
+    console.log('adding ' + code + ' to ' + stocks);
+    stocks.push(code);
+    io.emit('add-stock', code);
   });
-  socket.on('remove-stock', code => {
-  	console.log('removing '+code+' from '+stocks);
-    const index = stocks.indexOf(code);
-  	stocks.splice(index, 1);
-  	io.emit('remove-stock', code );
-  })
+  socket.on('remove-stock', function (code) {
+    console.log('removing ' + code + ' from ' + stocks);
+    var index = stocks.indexOf(code);
+    stocks.splice(index, 1);
+    io.emit('remove-stock', code);
+  });
 });
